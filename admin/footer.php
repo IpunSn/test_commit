09081d2df30235b7aa43d505de6a04ee0c10352a
IpunSn <nguyennpps36508@fpt.edu.vn>
2023-12-07 22:14:18 +0700

    admin index header home footer

---
+++ admin/footer.php
@@ -0,0 +1,8 @@
+</div>
+
+        
+    </div>
+    <script src="https://unpkg.com/@themesberg/flowbite@latest/dist/flowbite.bundle.js"></script>
+   <script src="./js/index.js"></script>
+</body>
+</html>
---
+++ admin/home.php
@@ -0,0 +1,71 @@
+<main class="w-full px-6 py-9 rounded-[20px] bg-[#eee]">
+                <header class="flex items-center justify-between gap-4 flex-wrap">
+                    <div>
+
+                        <h1 class="text-4xl font-semibold mb-2.5">Dashboard</h1>
+                        <ul class="flex items-center gap-4 ">
+                            <li><span href="">Trang chủ</span></li> 
+                          
+                        </ul>
+                    </div>
+                    <div></div>
+                </header>
+               
+                <!-- hết header -->
+
+                <!--  -->
+                <ul class="mt-9 grid grid-cols-4 gap-6 ">
+                    <li class="px-6 py-6 rounded-3xl  flex items-center gap-6 cursor-pointer bg-white shadow-lg shadow-gray-300">
+                        <span class="w-20 h-20 rounded-[10px] flex justify-center items-center bg-blue-200">
+                            <i class='text-blue-500 bx bx-lg bx-package'></i>
+                        </span>
+                        <span>
+                            <h3 class="text-2xl">10 đơn hàng</h3>
+                            <p>Mới</p>
+                        </span>
+                        
+                    </li>
+
+                    <li class="px-6 py-6 rounded-3xl  flex items-center gap-6 cursor-pointer bg-white">
+                        <span class="w-20 h-20 rounded-[10px] flex justify-center items-center bg-blue-200">
+                            <i class='text-blue-500 bx bx-lg bx-package'></i>
+                        </span>
+                        <span>
+                            <h3 class="text-2xl">10 đơn hàng</h3>
+                            <p>Mới</p>
+                        </span>
+                        
+                    </li>
+
+
+                    <li class="px-6 py-6 rounded-3xl  flex items-center gap-6 cursor-pointer bg-white">
+                        <span class="w-20 h-20 rounded-[10px] flex justify-center items-center bg-blue-200">
+                            <i class='text-blue-500 bx bx-lg bx-package'></i>
+                        </span>
+                        <span>
+                            <h3 class="text-2xl">10 đơn hàng</h3>
+                            <p>Mới</p>
+                        </span>
+                        
+                    </li>
+
+
+
+                    <li class="px-6 py-6 rounded-3xl  flex items-center gap-6 cursor-pointer bg-white">
+                        <span class="w-20 h-20 rounded-[10px] flex justify-center items-center bg-blue-200">
+                            <i class='text-blue-500 bx bx-lg bx-package'></i>
+                        </span>
+                        <span>
+                            <h3 class="text-2xl">10 đơn hàng</h3>
+                            <p>Mới</p>
+                        </span>
+                        
+                    </li>
+
+
+
+                    
+                    
+                </ul>
+</main>
+
